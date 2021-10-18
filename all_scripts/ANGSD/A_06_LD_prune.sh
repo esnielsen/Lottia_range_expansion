be4ca4ea779790da7fa0be8595f034c5ac8ed4f4
esnielsen <42109213+esnielsen@users.noreply.github.com>
2021-10-18 14:20:12 -0700

    Create A_06_LD_prune.sh

---
+++ all_scripts/ANGSD/A_06_LD_prune.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --mail-user=esnielsen@ucdavis.edu
+#SBATCH --mail-type=ALL
+#SBATCH -J plink_LD
+#SBATCH -p RM-shared
+#SBATCH -t 1:00:00
+#SBATCH --ntasks-per-node=1
+set -e
+set -x
+# To Run
+# sbatch A_06_LD_prune.sh
+
+#Set up directory
+cd /ocean/projects/deb200006p/enielsen/LGwork
+
+# Call fastp package
+module load anaconda3
+eval "$(conda shell.bash hook)"
+conda activate plink
+
+source A_01_config.sh
+
+N_IND=$(wc -l 02_info/bam.filelist | cut -d " " -f 1)
+MIN_IND_FLOAT=$(echo "($N_IND * $PERCENT_IND)"| bc -l)
+MIN_IND=${MIN_IND_FLOAT%.*} 
+MAX_DEPTH=$(echo "($N_IND * $MAX_DEPTH_FACTOR)" |bc -l)
+
+WINDOW=100
+SNP=5
+VIF=2
+
+plink --tped angsd/09_plink/all_plink_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".tped \
+--tfam angsd/09_plink/all_plink_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".tfam --indep $WINDOW $SNP $VIF --allow-extra-chr \
+--out angsd/09_plink/all_plink_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".pruned --threads 1 
+
