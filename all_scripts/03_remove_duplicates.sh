fa5746a176c4ce17caa812bdc9e9760d470999ee
esnielsen <42109213+esnielsen@users.noreply.github.com>
2021-10-18 13:41:11 -0700

    Update 03_remove_duplicates.sh

--- all_scripts/03_remove_duplicates.sh
+++ all_scripts/03_remove_duplicates.sh
@@ -1,1 +1,34 @@
 
+#!/bin/bash
+#SBATCH --mail-user=esnielsen@ucdavis.edu
+#SBATCH --mail-type=ALL
+#SBATCH -J mark_dups
+#SBATCH -p RM
+#SBATCH -t 2:00:00
+set -e
+set -x
+# To Run
+# sbatch 03_remove_duplicates.sh
+
+#Set up directory
+cd /ocean/projects/deb200006p/enielsen/LGwork
+
+# Global variables
+ALIGNEDFOLDER="06_aligned"
+DEDUPFOLDER="07_deduplicated"
+METRICSFOLDER="98_metrics"
+
+# Copy script to log folder
+TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
+SCRIPT=$0
+NAME=$(basename $0)
+LOG_FOLDER="99_log_files"
+cp "$SCRIPT" "$LOG_FOLDER"/"$TIMESTAMP"_"$NAME"
+
+# Load needed modules
+module load picard/2.23.2
+
+# Remove duplicates from bam alignments in parallel
+ls -1 "$ALIGNEDFOLDER"/*.bam |
+    parallel -j 10 echo "Deduplicating sample {}" \; picard MarkDuplicates INPUT={} OUTPUT="$DEDUPFOLDER"/{/}.dedup.bam METRICS_FILE="$METRICSFOLDER"/metrics.txt VALIDATION_STRINGENCY=SILENT REMOVE_DUPLICATES=true ASSUME_SORTED=true
+#done
