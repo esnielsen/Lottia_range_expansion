101c461c7fb57c3765606905a065e4a2c1650a08
esnielsen <42109213+esnielsen@users.noreply.github.com>
2021-10-19 10:23:31 -0700

    Create A_3.5_pcangsd_vis.sh

---
+++ all_scripts/ANGSD/A_3.5_pcangsd_vis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --mail-user=esnielsen@ucdavis.edu
+#SBATCH --mail-type=ALL
+#SBATCH -J pcangsd.vis
+#SBATCH -p RM-shared
+#SBATCH -t 1:00:00
+#SBATCH --ntasks-per-node=1
+set -e
+set -x
+# To Run
+# sbatch A_4.5.5_pcangsd_vis.sh
+
+#Set up directory
+cd /ocean/projects/deb200006p/enielsen/LGwork
+
+#prepare variables - avoid to modify
+source A_01_config.sh
+
+Rscript angsd/Rscripts/visualise_pca.r "$MIN_MAF" "$PERCENT_IND"
