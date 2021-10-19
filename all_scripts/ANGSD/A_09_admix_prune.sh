65ecd2386bd5c4410b48768a24630ebd6cc28fd8
esnielsen <42109213+esnielsen@users.noreply.github.com>
2021-10-19 11:52:24 -0700

    Create A_09_admix_prune.sh

---
+++ all_scripts/ANGSD/A_09_admix_prune.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --mail-user=esnielsen@ucdavis.edu
+#SBATCH --mail-type=ALL
+#SBATCH -J angsd_admix_prune
+#SBATCH -p RM-shared
+#SBATCH -t 1:00:00
+#SBATCH --ntasks-per-node=2
+set -e
+set -x
+# To Run
+# sbatch A_05_admix_PRUNE.sh
+
+#Set up directory
+cd /ocean/projects/deb200006p/enielsen/LGwork
+
+# Call package
+module load anaconda3
+eval "$(conda shell.bash hook)"
+conda activate ANGSD
+
+NB_CPU=2 #change accordingly in SLURM header
+
+source A_01_config.sh
+
+
+##Run for MIN_K to MAX_K (from config.sh file)
+
+for i in $(seq $K_MIN $K_MAX)
+	do 
+	echo $i
+	NGSadmix -P $NB_CPU -likes angsd/03_saf_maf_gl_all/all_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR"_pruned.beagle.gz \
+	-minMaf $MIN_MAF -K $i -o angsd/05_ngs_admix/all_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR"_pruned_K"$i"
+	done
