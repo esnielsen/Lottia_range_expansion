ab8337237ea05d35f572d9fde4dc13af9e4c3755
esnielsen <42109213+esnielsen@users.noreply.github.com>
2021-10-19 16:05:14 -0700

    Create A_14C_thetas_sing.sh

---
+++ all_scripts/ANGSD/A_14C_thetas_sing.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --mail-user=esnielsen@ucdavis.edu
+#SBATCH --mail-type=ALL
+#SBATCH -J angsd_theta_sing
+#SBATCH -p RM-shared
+#SBATCH -t 4:00:00
+#SBATCH --ntasks-per-node=8
+set -e
+set -x
+# To Run
+# sbatch A_7_thetas_sing.sh
+
+#Set up directory
+cd /ocean/projects/deb200006p/enielsen/LGwork
+
+chmod +x A_14C_thetas.sh
+
+singularity exec /ocean/projects/deb200006p/enielsen/LGwork/singularity-recipes_angsd_v0.933.sif /ocean/projects/deb200006p/enielsen/LGwork/A_14C_thetas.sh
