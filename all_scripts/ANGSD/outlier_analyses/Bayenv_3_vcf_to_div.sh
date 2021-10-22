32f7f60b447ef4105f6aed514da24fc437fb811b
esnielsen <42109213+esnielsen@users.noreply.github.com>
2021-10-22 15:12:26 -0700

    Create Bayenv_3_vcf_to_div.sh

---
+++ all_scripts/ANGSD/outlier_analyses/Bayenv_3_vcf_to_div.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --mail-user=esnielsen@ucdavis.edu
+#SBATCH --mail-type=ALL
+#SBATCH -J vcf_2_div
+#SBATCH -p RM-shared
+#SBATCH -t 1:00:00
+#SBATCH --ntasks-per-node=1
+set -e
+set -x
+# To Run
+# sbatch O_vcf_div.sh
+
+#Set up directory
+cd /ocean/projects/deb200006p/enielsen/LGwork
+
+# Call package
+module load anaconda2
+eval "$(conda shell.bash hook)"
+conda activate pyvcf.py2
+
+python genomisc/popogeno/vcf_2_div.py all.text.vcf --popmap popMAP.txt --exclude_singletons -b
