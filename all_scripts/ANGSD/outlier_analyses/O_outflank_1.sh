8374380b52dd969a409392384ea169b452c805df
esnielsen <42109213+esnielsen@users.noreply.github.com>
2021-10-22 15:07:06 -0700

    Create O_outflank_1.sh

---
+++ all_scripts/ANGSD/outlier_analyses/O_outflank_1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --mail-user=esnielsen@ucdavis.edu
+#SBATCH --mail-type=ALL
+#SBATCH -J outflank_1
+#SBATCH -p RM-shared
+#SBATCH -t 1:00:00
+#SBATCH --ntasks-per-node=1
+set -e
+set -x
+# To Run
+# sbatch O_outflank_1.sh
+
+#Set up directory
+cd /ocean/projects/deb200006p/enielsen/LGwork
+
+source A_01_config.sh
+
+Rscript 10_Rfiles/OutFLANKpairwise_ENmod.R angsd/06_saf_maf_by_pop/BMR/BMR_maf0.05_pctind0.5_maxdepth152.mafs angsd/06_saf_maf_by_pop/C/C_maf0.05_pctind0.5_maxdepth152.mafs angsd/06_saf_maf_by_pop/KR/KR_maf0.05_pctind0.5_maxdepth152.mafs angsd/06_saf_maf_by_pop/SP/SP_maf0.05_pctind0.5_maxdepth152.mafs BMR_C_KR_SP_OutFLANKin.152.txt
