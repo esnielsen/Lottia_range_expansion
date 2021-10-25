d2e423cac2baee605db0a279409aa09b43402f44
esnielsen <42109213+esnielsen@users.noreply.github.com>
2021-10-25 13:40:03 -0700

    Update A_02.B_get_sites.sh

--- all_scripts/ANGSD/A_02.B_get_sites.sh
+++ all_scripts/ANGSD/A_02.B_get_sites.sh
@@ -7,19 +7,19 @@
 set -e
 set -x
 # To Run
-# sbatch A_2.5_get_sites.sh
+# sbatch A_02.B_get_sites.sh
 
 #Set up directory
 cd /ocean/projects/deb200006p/enielsen/LGwork
 
-# Call fastp package
+# Call package
 module load anaconda3
 eval "$(conda shell.bash hook)"
 conda activate ANGSD
 
 ## Run this script if you just want to get the sites and region files for MAF scripts
 
-source A_01_config.sh
+source 01_config.sh
 
 echo "from the maf file, extract a list of SNP chr, positoin, major all, minor all"
 #gunzip angsd/03_saf_maf_gl_all/all_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".mafs.gz 
