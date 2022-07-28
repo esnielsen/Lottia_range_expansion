72bcb9207919ccf26e01ce6a9bc1da5948932e1e
esnielsen <42109213+esnielsen@users.noreply.github.com>
2022-07-28 11:35:09 +0200

    Update and rename A_06_plink_prune.sh to A_06_plink_R2_prune.sh

--- all_scripts/ANGSD/A_06_plink_R2_prune.sh
+++ all_scripts/ANGSD/A_06_plink_R2_prune.sh
@@ -8,7 +8,7 @@
 set -e
 set -x
 # To Run
-# sbatch A_06_plink_prune.sh
+# sbatch A_06_plink_R2_prune.sh
 
 #Set up directory
 cd /ocean/projects/deb200006p/enielsen/LGwork
@@ -18,18 +18,18 @@ module load anaconda3
 eval "$(conda shell.bash hook)"
 conda activate plink
 
-source A_01_config.sh
+source A_01.A_config.sh
 
 N_IND=$(wc -l 02_info/bam.filelist | cut -d " " -f 1)
 MIN_IND_FLOAT=$(echo "($N_IND * $PERCENT_IND)"| bc -l)
 MIN_IND=${MIN_IND_FLOAT%.*} 
 MAX_DEPTH=$(echo "($N_IND * $MAX_DEPTH_FACTOR)" |bc -l)
 
-WINDOW=100
-SNP=5
-VIF=2
+WINDOW=25
+STEP=10
+R=0.5
 
-plink --tped angsd/09_plink/all_plink_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".tped \
---tfam angsd/09_plink/all_plink_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".tfam --indep $WINDOW $SNP $VIF --allow-extra-chr \
---out angsd/09_plink/all_plink_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".pruned --threads 1 
+plink --tped angsd/plink/all_plink_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".tped \
+--tfam angsd/plink/all_plink_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".tfam --indep-pairwise $WINDOW $STEP $R --allow-extra-chr \
+--out angsd/plink/all_plink_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".R2.pruned --threads 1 
 
