9567a103ffc1cd4e636801f0c9326cedf09ca7a6
esnielsen <42109213+esnielsen@users.noreply.github.com>
2022-07-28 11:45:48 +0200

    Update A_13B_fst_pop_R.sh

--- all_scripts/ANGSD/A_13B_fst_pop_R.sh
+++ all_scripts/ANGSD/A_13B_fst_pop_R.sh
@@ -14,14 +14,14 @@ set -x
 cd /ocean/projects/deb200006p/enielsen/LGwork
 
 #prepare variables - avoid to modify
-source A_01_config.sh
+source A_01.A_config.sh
 
 #maybe edit
 NB_CPU=1 #change accordingly in SLURM header
 NSITES=500000 
 GROUP=pop 
-POP_FILE1=angsd/02_info/"$GROUP".txt #choose on which list of pop run the analyses
-
+POP_FILE1=02_info/"$GROUP".txt #choose on which list of pop run the analyses
+num_pops=4
 
 # Run pairwise FST R script for all populations listed
 
@@ -35,11 +35,11 @@ do
 		echo "$pop1"
 		echo "$pop2"
     
-file=07_fst_by_pop_pair/$GROUP/"$pop1"_"$pop2"_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR"."$NSITES"
+file=angsd/07_fst_by_pop_pair/$GROUP/"$pop1"_"$pop2"_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR"."$NSITES"
 
-Rscript 01_scripts/Rscripts/sum_sites_2dsfs.r "$file"
+Rscript Rscripts/sum_sites_2dsfs.r "$file"
 
-##Or if you want to just run on single population pair...
-file=angsd/07_fst_by_pop_pair/$GROUP/fold_nMAF/BMR_C_maf0.05_pctind0.5_maxdepth4_fold_nMAF.500000
+done
 
-Rscript angsd/Rscripts/sum_sites_2dsfs.r "$file"
+done
+
