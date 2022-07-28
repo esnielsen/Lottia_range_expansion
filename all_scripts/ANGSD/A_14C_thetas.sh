bfc12a246ecc1c5ca32fa1c469f5ba7e708bc0ac
esnielsen <42109213+esnielsen@users.noreply.github.com>
2022-07-28 11:48:14 +0200

    Update A_14C_thetas.sh

--- all_scripts/ANGSD/A_14C_thetas.sh
+++ all_scripts/ANGSD/A_14C_thetas.sh
@@ -3,7 +3,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH -J angsd_theta
 #SBATCH -p RM-shared
-#SBATCH -t 4:00:00
+#SBATCH -t 20:00:00
 #SBATCH --ntasks-per-node=8
 set -e
 set -x
@@ -17,9 +17,9 @@ cd /ocean/projects/deb200006p/enielsen/LGwork
 NB_CPU=8 #change accordingly in SLURM header
 NSITES=500000 #to make realSFS goes faster -reduce the number of sites considered
 GROUP=pop #the subgroup on whcih we are making the fst comparison -> it should be a file like GROUP.txt in the folder 02_info
-POP_FILE1=angsd/02_info/"$GROUP".txt #choose on which list of pop run the analyses
+POP_FILE1=02_info/"$GROUP".txt #choose on which list of pop run the analyses
 
-source A_01_config.sh
+source A_01.A_config.sh
 
 # Do thetas for all population listed
 
@@ -29,8 +29,8 @@ do
 
 	echo "estimate thetas for pop $i"
 	angsd -P $NB_CPU -dosaf 1 -doThetas 1 -GL 2 -doMajorMinor 1 -underFlowProtect 1 \
-	-anc 03_genome/Lottia_gigantea.Lotgi1.dna.toplevel.fa -uniqueOnly 1 -remove_bads 1 -skipTriallelic 1 -only_proper_pairs 1 -minMapQ 30 -minQ 20 -minInd 3 \
-	-b angsd/07_fst_by_pop_pair/$GROUP/"$i"subsetbam.filelist \
+	-anc 03_genome/Lottia_gigantea.Lotgi1.dna.toplevel.fa -uniqueOnly 1 -remove_bads 1 -skipTriallelic 1 -only_proper_pairs 1 -minMapQ 30 -minQ 20 -minInd 10 \
+	-b 02_info/"$i".bam.filelist \
 	-pest angsd/08_thetas/"$i"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR"."$NSITES".2dsfs \
 	-out angsd/08_thetas/"$i"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR"
 
