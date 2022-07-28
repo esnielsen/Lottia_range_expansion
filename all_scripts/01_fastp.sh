f5c786ca27b84e1ed5bca902ed2105e3474f1982
esnielsen <42109213+esnielsen@users.noreply.github.com>
2022-07-28 10:28:22 +0200

    Update 01_fastp.sh

--- all_scripts/01_fastp.sh
+++ all_scripts/01_fastp.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH -J fast-p
 #SBATCH -p RM-shared
-#SBATCH -t 2:00:00
+#SBATCH -t 20:00:00
 #SBATCH --ntasks-per-node=6
 set -e
 set -x
@@ -20,17 +20,29 @@ eval "$(conda shell.bash hook)"
 conda activate fastp.filter
 
 # Iterate over files in data folder
-for file in $(ls -1 40_raw_data/*_R1*.gz)
+for file in $(ls -1 rawdata/fastqs/*_*_1*.gz)
 do
-    input_file=$(echo "$file" | perl -pe 's/_R1.*\.fastq.gz//')
+    input_file=$(echo "$file" | perl -pe 's/_1.fq.gz//')
     output_file=$(basename "$input_file")
     echo "Treating: $output_file"
 
     fastp -w 6 \
-        -i "$input_file"_R1.fastq.gz \
-        -I "$input_file"_R2.fastq.gz \
-        -o 05_trimmed/"$output_file"_1.trimmed.fastq.gz \
-        -O 05_trimmed/"$output_file"_2.trimmed.fastq.gz \
-        -j 05_trimmed/01_reports/"$output_file".json \
-        -h 05_trimmed/01_reports/"$output_file".html
+        -i "$input_file"_1.fq.gz \
+        -I "$input_file"_2.fq.gz \
+        -o 02_FP_A_trimmed/"$output_file"_1.trimmed.fq.gz \
+        -O 02_FP_A_trimmed/"$output_file"_2.trimmed.fq.gz \
+        --adapter_sequence=AAGTCGGAGGCCAAGCGGTCTTAGGAAGACAA \
+        --adapter_sequence_r2=AAGTCGGATCGTAGCCATGTCGTTCTGTGAGCCAAGGAGTTG \
+        -j 02_FP_A_trimmed/01_reports/"$output_file".json \
+        -h 02_FP_A_trimmed/01_reports/"$output_file".html
 done
+
+# w = number of threads
+# i = read 1
+# I = read 2
+# o & O = outputs
+# outputs .json and html for qc
+
+# Run for single file
+#fastp -w 6 -i rawdata/fastqs/SBR_33_1.fq.gz -I rawdata/fastqs/SBR_33_2.fq.gz --adapter_sequence=AAGTCGGAGGCCAAGCGGTCTTAGGAAGACAA --adapter_sequence_r2=AAGTCGGATCGTAGCCATGTCGTTCTGTGAGCCAAGGAGTTG -o 02_FP_A_trimmed/SBR_33_1.trimA.fq.gz -O 02_FP_A_trimmed/SBR_33_2.trimA.fq.gz -j 02_FP_A_trimmed/01_reports/SBR_33_A.json -h 02_FP_A_trimmed/01_reports/SBR_33_A.html 
+
