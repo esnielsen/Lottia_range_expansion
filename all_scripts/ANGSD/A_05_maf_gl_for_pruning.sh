11dc6c9f449ea20591085195c06581e367fe54a8
esnielsen <42109213+esnielsen@users.noreply.github.com>
2022-07-28 11:33:42 +0200

    Update A_05_maf_gl_for_pruning.sh

--- all_scripts/ANGSD/A_05_maf_gl_for_pruning.sh
+++ all_scripts/ANGSD/A_05_maf_gl_for_pruning.sh
@@ -2,26 +2,26 @@
 #SBATCH --mail-user=esnielsen@ucdavis.edu
 #SBATCH --mail-type=ALL
 #SBATCH -J angsd_LD
-#SBATCH -p RM-shared
-#SBATCH -t 1:00:00
-#SBATCH --ntasks-per-node=1
+#SBATCH -p EM
+#SBATCH -t 28:00:00
+#SBATCH --ntasks-per-node=24
 set -e
 set -x
 # To Run
-# sbatch A_05_maf_gl_for_pruning.sh
+# sbatch A_05_gl_for_plink.sh
 
 #Set up directory
 cd /ocean/projects/deb200006p/enielsen/LGwork
 
-NB_CPU=1 #change accordingly in SLURM header
-REGIONS="-rf angsd/02_info/regions_all_maf0.05_pctind0.5_maxdepth4"
+NB_CPU=24 #change accordingly in SLURM header
+REGIONS="-rf 02_info/regions_all_maf0.05_pctind0.5_maxdepth30"
 
-source A_01_config.sh
+source A_01.A_config.sh
 
 ####Calculate the MAF and GL, with Plink output for LD pruning
 
 angsd -P $NB_CPU -nQueueSize 50 -doPlink 2 -doMaf 1 -doCounts 1 \
 -GL 2 -doMajorMinor 1 -doGeno -4 -doPost 1 -postCutoff 0.8 \
 -anc 03_genome/Lottia_gigantea.Lotgi1.dna.toplevel.fa -uniqueOnly 1 -remove_bads 1 -skipTriallelic 1 -only_proper_pairs 1 -minMapQ 30 -minQ 20 $REGIONS \
--minInd 3 -minMaf $MIN_MAF -setMaxDepth 12 -b angsd/02_info/bam.filelist \
--out angsd/09_plink/all_plink_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR"
+-minInd 150 -minMaf $MIN_MAF -setMaxDepth 960 -b 02_info/bam.filelist \
+-out angsd/plink/all_plink_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR"
