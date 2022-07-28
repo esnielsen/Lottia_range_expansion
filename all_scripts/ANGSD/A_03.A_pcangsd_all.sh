fa187c82f6c09dcfd020031cf4941e75be247e18
esnielsen <42109213+esnielsen@users.noreply.github.com>
2022-07-28 11:28:34 +0200

    Update A_03.A_pcangsd_all.sh

--- all_scripts/ANGSD/A_03.A_pcangsd_all.sh
+++ all_scripts/ANGSD/A_03.A_pcangsd_all.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH -J pcangsd
 #SBATCH -p RM-shared
-#SBATCH -t 1:00:00
+#SBATCH -t 6:00:00
 #SBATCH --ntasks-per-node=1
 set -e
 set -x
@@ -14,7 +14,7 @@ set -x
 #Set up directory
 cd /ocean/projects/deb200006p/enielsen/LGwork
 
-module load python3.8.6
+#module load python3.8.6
 
 # Call package
 module load anaconda3
@@ -24,18 +24,17 @@ conda activate numpy
 NB_CPU=1 #change accordingly in SLURM header
 
 #prepare variables - avoid to modify
-source A_01_config.sh
+source A_01.A_config.sh
 
 #this is the list of file we are working on
-BAM_LIST=angsd/02_info/bam.filelist
+BAM_LIST=02_info/CA.bam.filelist
 
 #this is the input file for the pca
-INPUT=angsd/03_saf_maf_gl_all/all_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".beagle.gz
+INPUT=angsd/03_saf_maf_gl_all/CA_all_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".beagle.gz
 
 echo "analyse covariance matrix on all individuals"
 python3 $PCA_ANGSD_PATH/pcangsd.py -threads $NB_CPU \
-	-beagle $INPUT -o angsd/04_pca/all_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR"
+	-beagle $INPUT -o angsd/04_pca/CA_all_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR"
 
 echo "transform covariance matrix into PCA"
-COV_MAT=angsd/04_pca/all_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".cov
-Rscript angsd/Rscripts/make_pca_simple.r "$COV_MAT" "$BAM_LIST"
+COV_MAT=angsd/04_pca/CA_all_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".cov
