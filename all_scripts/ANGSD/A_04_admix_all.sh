fe305cc0feaf4095d31467af389de2921a5b6e9a
esnielsen <42109213+esnielsen@users.noreply.github.com>
2022-07-28 11:30:30 +0200

    Update A_04_admix_all.sh

--- all_scripts/ANGSD/A_04_admix_all.sh
+++ all_scripts/ANGSD/A_04_admix_all.sh
@@ -4,12 +4,12 @@
 #SBATCH --mail-type=ALL
 #SBATCH -J angsd_admix
 #SBATCH -p RM-shared
-#SBATCH -t 1:00:00
-#SBATCH --ntasks-per-node=2
+#SBATCH -t 48:00:00
+#SBATCH --ntasks-per-node=20
 set -e
 set -x
 # To Run
-# sbatch A_04_admix_all.sh
+# sbatch A_04.A_admix_all.sh
 
 #Set up directory
 cd /ocean/projects/deb200006p/enielsen/LGwork
@@ -19,17 +19,18 @@ module load anaconda3
 eval "$(conda shell.bash hook)"
 conda activate ANGSD
 
-NB_CPU=2 #change accordingly in SLURM header
+NB_CPU=20 #change accordingly in SLURM header
 
-source A_01_config.sh
+source A_01.A_config.sh
 
 
-## Run admixture for Kmin to Kmax (these set in the config file)
+## Run admixture for Kmin to Kmax (these set in the config file) 10 times each
+for j in {1..10}; do
 
-for i in $(seq $K_MIN $K_MAX)
-	do 
-	echo $i
-	NGSadmix -P $NB_CPU -likes angsd/03_saf_maf_gl_all/all_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".beagle.gz \
-	-minMaf $MIN_MAF -K $i -o angsd/05_ngs_admix/all_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR"_K"$i"
+	for i in $(seq $K_MIN $K_MAX)
+		do 
+		echo $i
+		NGSadmix -P $NB_CPU -likes angsd/03_saf_maf_gl_all/CA_all_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".beagle.gz \
+		-minMaf $MIN_MAF -K $i -o angsd/05_ngs_admix/CA_all_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR"_K"$i"_run"$j"
 	done
-	
+done
