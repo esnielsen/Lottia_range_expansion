a6b78b75573021e55a629d706db1d69906128802
esnielsen <42109213+esnielsen@users.noreply.github.com>
2022-07-28 11:28:59 +0200

    Update A_03.B_pcangsd_vis.sh

--- all_scripts/ANGSD/A_03.B_pcangsd_vis.sh
+++ all_scripts/ANGSD/A_03.B_pcangsd_vis.sh
@@ -14,6 +14,11 @@ set -x
 cd /ocean/projects/deb200006p/enielsen/LGwork
 
 #prepare variables - avoid to modify
-source A_01_config.sh
+source A_01.A_config.sh
 
-Rscript angsd/Rscripts/visualise_pca.r "$MIN_MAF" "$PERCENT_IND"
+BAM_LIST=02_info/CA.bam.filelist
+COV_MAT=angsd/04_pca/CA_all_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".cov
+
+Rscript Rscripts/make_pca_simple.r "$COV_MAT" "$BAM_LIST"
+
+Rscript Rscripts/visualise_pca.r "$MIN_MAF" "$PERCENT_IND"
