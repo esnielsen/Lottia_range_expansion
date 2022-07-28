12ee370492d967261ffb1fbf1855dec71febf4be
esnielsen <42109213+esnielsen@users.noreply.github.com>
2022-07-28 11:39:25 +0200

    Update A_09_admix_prune.sh

--- all_scripts/ANGSD/A_09_admix_prune.sh
+++ all_scripts/ANGSD/A_09_admix_prune.sh
@@ -3,8 +3,8 @@
 #SBATCH --mail-type=ALL
 #SBATCH -J angsd_admix_prune
 #SBATCH -p RM-shared
-#SBATCH -t 1:00:00
-#SBATCH --ntasks-per-node=2
+#SBATCH -t 48:00:00
+#SBATCH --ntasks-per-node=6
 set -e
 set -x
 # To Run
@@ -18,15 +18,19 @@ module load anaconda3
 eval "$(conda shell.bash hook)"
 conda activate ANGSD
 
-NB_CPU=2 #change accordingly in SLURM header
+NB_CPU=6 #change accordingly in SLURM header
 
-source A_01_config.sh
+source A_01.A_config.sh
 
-##Run for MIN_K to MAX_K (from config.sh file)
 
-for i in $(seq $K_MIN $K_MAX)
-	do 
-	echo $i
+## Run admixture for Kmin to Kmax (these set in the config file) 10 times each
+for j in {1..10}; do
+
+	for i in $(seq $K_MIN $K_MAX)
+		do 
+		echo $i
 	NGSadmix -P $NB_CPU -likes angsd/03_saf_maf_gl_all/all_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR"_pruned.beagle.gz \
-	-minMaf $MIN_MAF -K $i -o angsd/05_ngs_admix/all_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR"_pruned_K"$i"
+	-minMaf $MIN_MAF -K $i -o angsd/05_ngs_admix/pruned/all_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR"_pruned_K"$i"_run"$j"
 	done
+done
+
