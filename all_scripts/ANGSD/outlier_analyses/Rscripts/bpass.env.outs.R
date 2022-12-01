e78a538cc86a5bb1bc05e14df671efd5a7f345da
esnielsen <42109213+esnielsen@users.noreply.github.com>
2022-12-01 12:05:54 +0200

    Update bpass.env.outs.R

--- all_scripts/ANGSD/outlier_analyses/Rscripts/bpass.env.outs.R
+++ all_scripts/ANGSD/outlier_analyses/Rscripts/bpass.env.outs.R
@@ -4,12 +4,12 @@ library(dplyr)
 
 setwd("/ocean/projects/deb200006p/enielsen/LGwork/Outliers/03_baypass")
 
-dB.df=read.table("ALL_2_r2.env.output_summary_betai_reg.out",h=T)
+dB.df=read.table("ALL_2_r2.env.output_summary_betai_reg.out",h=T) #read output table from baypass
 
-names(dB.df)[7] <- "dB"
+names(dB.df)[7] <- "dB" #rename the 7th column as dB
 
-dB.df.outs <- dB.df %>% filter(dB>20)
+dB.df.outs <- dB.df %>% filter(dB>20) #filter the column to only show snps with a dB greater than 20 (this is common significance cut-off for this analysis)
 
-bpass.env.out.list = subset(dB.df.outs , select = -c(3:6,8:10) )
+bpass.env.out.list = subset(dB.df.outs , select = -c(3:6,8:10) ) #subset dataframe 
 
 write.table(bpass.env.out.list, "bpass.env.out.list.txt", row.names=FALSE, sep="\t", quote = FALSE)
