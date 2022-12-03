9463f42c4cacb602b2b9e65f66ca8cb7efabc0d7
esnielsen <42109213+esnielsen@users.noreply.github.com>
2022-12-03 13:59:16 +0200

    Update RE_2_make_bed.sh

--- all_scripts/ANGSD/migration_analyses/RE_2_make_bed.sh
+++ all_scripts/ANGSD/migration_analyses/RE_2_make_bed.sh
@@ -25,11 +25,11 @@ MIN_IND_FLOAT=$(echo "($N_IND * $PERCENT_IND)"| bc -l)
 MIN_IND=${MIN_IND_FLOAT%.*} 
 MAX_DEPTH=$(echo "($N_IND * $MAX_DEPTH_FACTOR)" |bc -l)
 
-plink --tped angsd/09_plink/all_plink_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".tped \
---tfam angsd/09_plink/all_plink_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".tfam --allow-extra-chr \
---make-bed --out angsd/09_plink/all_plink_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".bed
+#plink --tped angsd/09_plink/all_plink_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".tped \
+#--tfam angsd/09_plink/all_plink_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".tfam --allow-extra-chr \
+#--make-bed --out angsd/09_plink/all_plink_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".bed
 
-#plink --file angsd/09_plink/all_plink_maf0.05_pctind0.5_maxdepth4.bin --allow-extra-chr \
-#--make-bed --out angsd/09_plink/all_plink_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".bin
+plink --file angsd/09_plink/all_plink_maf0.05_pctind0.5_maxdepth4.bin --allow-extra-chr \
+--make-bed --out angsd/09_plink/all_plink_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".bin
 
 #plink --file angsd/09_plink/all_plink_maf0.05_pctind0.5_maxdepth4.bin --allow-extra-chr --recode vcf
