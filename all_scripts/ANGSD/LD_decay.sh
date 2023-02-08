c16ce8a03e15e1adba963ced968c48f92da165d9
esnielsen <42109213+esnielsen@users.noreply.github.com>
2023-02-08 13:01:15 -0800

    Update LD_decay.sh

--- all_scripts/ANGSD/LD_decay.sh
+++ all_scripts/ANGSD/LD_decay.sh
@@ -13,8 +13,11 @@ set -x
 #Set up directory
 cd /ocean/projects/deb200006p/enielsen/LGwork
 
+#use perl script with 2 inputs (both plink - the .ped and .map files) 
 perl LD/PopLDdecay/bin/mis/plink2genotype.pl -inPED angsd/03_saf_maf_gl_all/LD_core.ped -inMAP angsd/03_saf_maf_gl_all/LD_core.map -outGenotype LD/core.genotype
 
+#second step of process, use MAF filtering of 0.05
 LD/PopLDdecay/bin/PopLDdecay -InGenotype LD/core.genotype -OutStat LD/core.LD.stat.gz -MAF 0.05
 
+#Plot the LD curve for a single pop
 perl LD/PopLDdecay/bin/Plot_OnePop.pl -inFile LD/core.LD.stat.gz -output LD/core.pdf
