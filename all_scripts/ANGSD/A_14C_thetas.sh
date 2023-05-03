ab5c7f14bdd6f752eac0948450c85313dd5ae1a5
esnielsen <42109213+esnielsen@users.noreply.github.com>
2023-05-03 15:26:54 -0700

    Update A_14C_thetas.sh

--- all_scripts/ANGSD/A_14C_thetas.sh
+++ all_scripts/ANGSD/A_14C_thetas.sh
@@ -16,8 +16,7 @@ cd /ocean/projects/deb200006p/enielsen/LGwork
 
 NB_CPU=8 #change accordingly in SLURM header
 NSITES=500000 #to make realSFS goes faster -reduce the number of sites considered
-GROUP=pop #the subgroup on whcih we are making the fst comparison -> it should be a file like GROUP.txt in the folder 02_info
-POP_FILE1=02_info/"$GROUP".txt #choose on which list of pop run the analyses
+POP_FILE1=02_info/pop.txt #choose on which list of pop run the analyses
 
 source A_01.A_config.sh
 
@@ -27,6 +26,7 @@ cat $POP_FILE1 | while read i
 do
 	echo $i
 
+#now we use the sfs and the function -doThetas to get the theta output
 	echo "estimate thetas for pop $i"
 	angsd -P $NB_CPU -dosaf 1 -doThetas 1 -GL 2 -doMajorMinor 1 -underFlowProtect 1 \
 	-anc 03_genome/Lottia_gigantea.Lotgi1.dna.toplevel.fa -uniqueOnly 1 -remove_bads 1 -skipTriallelic 1 -only_proper_pairs 1 -minMapQ 30 -minQ 20 -minInd 10 \
@@ -34,7 +34,7 @@ do
 	-pest angsd/08_thetas/"$i"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR"."$NSITES".2dsfs \
 	-out angsd/08_thetas/"$i"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR"
 
-	#Do a sliding window analysis based on the output from the make_bed command.
+	#Do a sliding window analysis
 	thetaStat do_stat angsd/08_thetas/"$i"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".thetas.idx -win $WINDOW -step $WINDOW_STEP \
 	-outnames angsd/08_thetas/"$i"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR".thetaswindow
 
