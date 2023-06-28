23ed046ac08749c3b6b245b9767ee747a6a75a9c
esnielsen <42109213+esnielsen@users.noreply.github.com>
2023-06-28 15:24:18 -0700

    Update M_1_create_sfs.sh

--- all_scripts/ANGSD/AFS_Moments/M_1_create_sfs.sh
+++ all_scripts/ANGSD/AFS_Moments/M_1_create_sfs.sh
@@ -13,14 +13,17 @@ set -x
 #Set up directory
 cd /ocean/projects/deb200006p/enielsen/LGwork
 
-GRate=0.8 # genotyping rate filter - a site must be genotyped in this fraction of all samples.
-#NB: when I do the cat below, go to the file and delete the empty row, and get # of inds, then calculate 80% to get minIND
-#cat 02_info/BMRbam.filelist 02_info/Cbam.filelist > p12.bams
-
 #NB: make sure you use the pruned sites list to create sfs
-FILTERS="-uniqueOnly 1 -skipTriallelic 1 -minMapQ 30 -minQ 25 -doHWE 1 -hetbias_pval 1e-5 -minInd 14 -sb_pval 1e-5"
-# I deleted -maxHetFreq 0.5 because with this small sample size the heterozygosity was too low
+#below I have 2 comparisons: CA.n.c (CA north vs. core) and CA.s.Mex (CA south vs. Mex)
+
+FILTERS="-uniqueOnly 1 -skipTriallelic 1 -minMapQ 30 -minQ 20 -doHWE 1 -hetbias_pval 1e-5 -minInd 120 -sb_pval 1e-5"
+# I didn't use -maxHetFreq 0.5 because it didn't work with version of angsd
 TODO='-doMajorMinor 1 -doMaf 1 -dosnpstat 1 -doPost 2'
-angsd -b angsd/p12.bams -sites angsd/02_info/sites_all_maf0.05_pctind0.5_maxdepth4_pruned -GL 1 $FILTERS $TODO -P 4 -out 0.1_unlinked.sites
-zcat 0.1_unlinked.sites.mafs.gz | cut -f 1,2 | tail -n +2 > finalsites_0.1_unlinked
-angsd sites index finalsites_0.1_unlinked
+
+angsd -b 02_info/CA.s.Mex.bam.filelist -sites 02_info/sites_ALL_2_maf0.05_pctind0.5_maxdepth15_R2.pruned -GL 1 -P 40 $FILTERS $TODO -out Moments/CA.s.Mex.sites 
+zcat Moments/CA.s.Mex.sites.mafs.gz | cut -f 1,2 | tail -n +2 > Moments/finalsites_CA.s.Mex
+angsd sites index Moments/finalsites_CA.s.Mex
+
+angsd -b 02_info/CA.n.c.bam.filelist -sites 02_info/sites_ALL_2_maf0.05_pctind0.5_maxdepth15_R2.pruned -GL 1 -P 40 $FILTERS $TODO -out Moments/CA.n.c.sites 
+zcat Moments/CA.n.c.sites.mafs.gz | cut -f 1,2 | tail -n +2 > Moments/finalsites_CA.n.c
+angsd sites index Moments/finalsites_CA.n.c
