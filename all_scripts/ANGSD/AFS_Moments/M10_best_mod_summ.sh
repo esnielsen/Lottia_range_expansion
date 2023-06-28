5052de01070cd0c5a0b2f2a23a1f6f193c6e3007
esnielsen <42109213+esnielsen@users.noreply.github.com>
2023-06-28 15:49:19 -0700

    Update M10_best_mod_summ.sh

--- all_scripts/ANGSD/AFS_Moments/M10_best_mod_summ.sh
+++ all_scripts/ANGSD/AFS_Moments/M10_best_mod_summ.sh
@@ -8,9 +8,15 @@
 set -e
 set -x
 # To Run
-# sbatch M_best_mod.sh
+# sbatch M10_best_mod_summ.sh
 
+# Call package
+module load anaconda3
+conda activate moments
 
 cd /ocean/projects/deb200006p/enielsen/LGwork/Moments
 
-Rscript AFS-analysis-with-moments/work/bestBoot_summary.EN.R bootRes=AFS-analysis-with-moments/p12.winboots
+# now we run this R script to summarize the parameters of the winning model and plot some plots
+# here I include an editted version of the bestBoot_summary.R script -- it wasn't picking up the model parameters so I manually added them based on the model's python script
+
+Rscript AFS-analysis-with-moments/work/bestBoot_summary.EN.2.R bootRes=pCA.n.c.NEW.winboots folded=TRUE
