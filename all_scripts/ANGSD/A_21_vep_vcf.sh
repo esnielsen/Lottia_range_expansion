0d8f33be38e089221915ae3a44c8a8450f8071dd
esnielsen <42109213+esnielsen@users.noreply.github.com>
2023-06-28 09:45:04 -0700

    Update A_21_vep_vcf.sh

--- all_scripts/ANGSD/A_21_vep_vcf.sh
+++ all_scripts/ANGSD/A_21_vep_vcf.sh
@@ -27,8 +27,9 @@ NB_CPU=40 #change accordingly in SLURM header
 source A_01.A_config.sh
 
 angsd -P $NB_CPU -nQueueSize 50 \
--dosaf 1 -GL 2 -doGlf 2 -doMajorMinor 1 -doMaf 1 -doGeno 2 -doCounts 1 -doPost 1 -doBcf 1 -doPlink 2 -postCutoff 0.8 \
+-doMaf 1 -dosaf 1 -GL 2 -doGlf 2 -doMajorMinor 3 -doGeno 2 -doCounts 1 -doPost 1 -dovcf 1 -doPlink 2 -postCutoff 0.8 \
 -anc 03_genome/Lottia_gigantea.Lotgi1.dna.toplevel.fa \
 -minMapQ 30 -minQ 20 -minInd 165 -setMaxDepth 3100 -uniqueOnly 1 -remove_bads 1 -only_proper_pairs 1 -skipTriallelic 1 \
--b 02_info/vep.bams.filelist \
--out angsd/03_saf_maf_gl_all/regions/vep_maf"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR"
+-sites 02_info/sites_ALL_2_maf0.05_pctind0.5_maxdepth15_R2.pruned -rf 02_info/regions_ALL_2_maf0.05_pctind0.5_maxdepth15 \
+-b 02_info/CA.n.core.bam.filelist \
+-out angsd/03_saf_maf_gl_all/regions/CA.n.core_"$MIN_MAF"_pctind"$PERCENT_IND"_maxdepth"$MAX_DEPTH_FACTOR"
