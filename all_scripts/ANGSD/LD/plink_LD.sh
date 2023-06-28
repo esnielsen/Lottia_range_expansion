8756d1f641adc15d7cfe365a0fffa8e8f951ba77
esnielsen <42109213+esnielsen@users.noreply.github.com>
2023-06-28 09:32:09 -0700

    Update plink_LD.sh

--- all_scripts/ANGSD/LD/plink_LD.sh
+++ all_scripts/ANGSD/LD/plink_LD.sh
@@ -20,7 +20,7 @@ conda activate plink
 
 
 # calc ld with plink
-# the tfile was created with A_19_saf_maf_regions.sh
+# the tfile was created with A_18_saf_maf_regions.sh
 plink --tfile angsd/03_saf_maf_gl_all/LD_trailing_maf0.05_pctind0.5_maxdepth15 --double-id --allow-extra-chr \
 --set-missing-var-ids @:# \
 --maf 0.05 --geno 0.1 --mind 0.5 \
