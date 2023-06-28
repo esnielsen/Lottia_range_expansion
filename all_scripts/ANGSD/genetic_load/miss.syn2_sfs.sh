7748a3c46ee3dda463a49a50759478ef39ce1ae1
esnielsen <42109213+esnielsen@users.noreply.github.com>
2023-06-28 15:10:32 -0700

    Update miss.syn2_sfs.sh

--- all_scripts/ANGSD/genetic_load/miss.syn2_sfs.sh
+++ all_scripts/ANGSD/genetic_load/miss.syn2_sfs.sh
@@ -13,19 +13,14 @@ set -x
 #Set up directory
 cd /ocean/projects/deb200006p/enielsen/LGwork/gen_load/ensembl-vep
 
-#create SFSs per pop based on the snps that are either missense or synonymous
-realSFS KR.nomaf.synon.saf.idx -P 4 > KR.nomaf.synon.sfs
-realSFS FR.nomaf.synon.saf.idx -P 4 > FR.nomaf.synon.sfs
-realSFS BB.nomaf.synon.saf.idx -P 4 > BB.nomaf.synon.sfs
-realSFS DB.nomaf.synon.saf.idx -P 4 > DB.nomaf.synon.sfs
-realSFS HP.nomaf.synon.saf.idx -P 4 > HP.nomaf.synon.sfs
-realSFS CR.nomaf.synon.saf.idx -P 4 > CR.nomaf.synon.sfs
-realSFS SB.nomaf.synon.saf.idx -P 4 > SB.nomaf.synon.sfs
+POP_FILE1=../../02_info/pop.txt #change with pops
 
-realSFS KR.nomaf.mis.saf.idx -P 4 > KR.nomaf.mis.sfs
-realSFS FR.nomaf.mis.saf.idx -P 4 > FR.nomaf.mis.sfs
-realSFS BB.nomaf.mis.saf.idx -P 4 > BB.nomaf.mis.sfs
-realSFS DB.nomaf.mis.saf.idx -P 4 > DB.nomaf.mis.sfs
-realSFS HP.nomaf.mis.saf.idx -P 4 > HP.nomaf.mis.sfs
-realSFS CR.nomaf.mis.saf.idx -P 4 > CR.nomaf.mis.sfs
-realSFS SB.nomaf.mis.saf.idx -P 4 > SB.nomaf.mis.sfs
+# Do saf/maf for all population listed
+cat $POP_FILE1 | while read i
+do
+echo $i
+
+realSFS "$i".nomaf.synon.saf.idx -P 4 > "$i".nomaf.synon.sfs
+realSFS "$i".nomaf.mis.saf.idx -P 4 > "$i".nomaf.mis.sfs
+
+done
