75bff5398428b806a91ee570933f64d679bf793f
esnielsen <42109213+esnielsen@users.noreply.github.com>
2023-06-28 15:35:12 -0700

    Update M5_bagging.sh

--- all_scripts/ANGSD/AFS_Moments/M5_bagging.sh
+++ all_scripts/ANGSD/AFS_Moments/M5_bagging.sh
@@ -8,13 +8,15 @@
 set -e
 set -x
 # To Run
-# sbatch M_bagging.sh
+# sbatch M5_bagging.sh
 
 #Set up directory
-cd /ocean/projects/deb200006p/enielsen/LGwork
+cd /ocean/projects/deb200006p/enielsen/LGwork/Moments/p2.CA.Mex
 
-SFSIZE="17 21" # 2N+1 for each population. In this case we assume that we have sampled 10 diploid individuals from each `p1` and `p2`.
+#this script will 'bag' the bootstraps
+
+SFSIZE="61 61" # 2N+1 for each population (randomly sampled each population to 30 individuals)
 for B in `seq 1 100`; do
-echo $SFSIZE >p12_${B}.sfs;
-cat p12_${B} | awk '{for (i=1;i<=NF;i++){a[i]+=$i;}} END {for (i=1;i<=NF;i++){printf "%.3f", a[i]/NR; printf "\t"};printf "\n"}' >> p12_${B}.sfs;
+echo $SFSIZE >CA.s.Mex_${B}.sfs;
+cat CA.s.Mex_${B} | awk '{for (i=1;i<=NF;i++){a[i]+=$i;}} END {for (i=1;i<=NF;i++){printf "%.3f", a[i]/NR; printf "\t"};printf "\n"}' >> CA.s.Mex_${B}.sfs;
 done
