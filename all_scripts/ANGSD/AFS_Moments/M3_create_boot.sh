56dcd24c2cc3a70cd7032b762b8af0186f9857b0
esnielsen <42109213+esnielsen@users.noreply.github.com>
2023-06-28 15:32:30 -0700

    Update M3_create_boot.sh

--- all_scripts/ANGSD/AFS_Moments/M3_create_boot.sh
+++ all_scripts/ANGSD/AFS_Moments/M3_create_boot.sh
@@ -8,12 +8,12 @@
 set -e
 set -x
 # To Run
-# sbatch M_boot_1.sh
+# sbatch M3_create_boot.sh
 
 #Set up directory
 cd /ocean/projects/deb200006p/enielsen/LGwork
 
-#bootstrap 100 times to account for variation in SFSs (here shown just for CA.s Mex comparison)
+# this script creates the script to bootstrap 100 times to account for variation in SFSs (here shown just for CA.s Mex comparison)
 >b100
 for B in `seq 1 100`; do
 echo "sleep $B && realSFS p2.CA.s.saf.idx p2.Mex.saf.idx -ref 03_genome/Lottia_gigantea.Lotgi1.dna.toplevel.fa -anc 03_genome/Lottia_gigantea.Lotgi1.dna.toplevel.fa -bootstrap 5 -P 10 -resample_chr 1 > p2.CA.Mex/CA.s.Mex_$B">>b100;
